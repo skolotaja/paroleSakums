5cfde76ef0b74a522a5ec8dc652aace8931f60c8
skolotaja <94806629+skolotaja@users.noreply.github.com>
2021-12-12 15:37:53 +0200

    turpinKludas

--- script.js
+++ script.js
@@ -1,26 +1,33 @@
-let tabulasRindas = document.querySelector('.rindas');
 let ciparuSkaits;
-let variantuSkaits;
+let variantuSkaits = 5;
 let papildusVards;
 
 const vardi1 = ['spalvains', 'karsta', 'papedis', 'mazais', 'pinkains'];
-const vardi2 = ['lacis', 'vista', 'ruksis', 'sunis', 'sesks', 'kakis'];
+const vardi2 = ['lacis', 'vista', 'ruksis', 'suns', 'sesks', 'kakis'];
 const vardi3 = ['lien', 'klukst', 'lec', 'lido', 'skrien', 'naud'];
+let rindas = document.querySelector('.rindas');
 function randFunc(maxCipars) {
-    random1 = Math.floor(Math.random() * maxCipars);
+   let random1 = Math.floor(Math.random() * maxCipars);
     return random1;
 }
-function generet() {
-    tabulasRindas.innerHTML = "";
+function generetParoles() {
+    rindas.innerHTML = "";
     for (let i = 0; i < variantuSkaits; i++) {
         let rand1 = randFunc(vardi1.length);
         let rand2 = randFunc(vardi2.length);
         let rand3 = randFunc(vardi3.length);
 
-        let parole1=vardi1[rand1];
-        let parole2=vardi2[rand2];
-        let parole3=vardi3[rand3];
-        let parole=parole1+parole2+parole3;
+        let parole1 = vardi1[rand1];
+        let parole2 = vardi2[rand2];
+        let parole3 = vardi3[rand3];
+
+//let random = Math.ceil(Math.random() * 10);
+        let parole = parol[i] + parole[i] + parole[i];
+        rindas.innerHTML += `
+        <tr>
+        <td>${i + 1}</td>
+        <td>${parole}</td>
+        </tr>`
 
 
     }
